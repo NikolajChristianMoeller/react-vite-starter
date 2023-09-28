c96b3a9c19d22d6cbf52be5ca4682bd8b6d215e3
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2023-09-28 13:10:12 +0200

    Initial commit

---
+++ components/Member.jsx
@@ -0,0 +1,5 @@
+export default function Member() {
+    return (
+        <p>This is a Member Component 🎉</p>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
 import reactLogo from "./assets/react.svg";
 
 function App() {
+    const [members, setMembers] = useState([]);
+    { firstName: "Peter", lastName: "Lind" },
+    { firstName: "Edith", lastName: "Terte"},
+    { firstName: "Oskar", lastName: "Tuska" }
+}); 
+
+    useEffect(()=> {
+        async function getMembers(){
+            const res = await fetch("https://raw.githubusercontent.com/cederdorff/dat-js/main/data/members.json");
+            const data = await res.json();
+            console.log(data):;
+
+            setMembers(data);
+
+        }
+
+        getMembers();
+    }, []);
+
     return (
         <>
             <header>
@@ -9,8 +28,17 @@ function App() {
                 </a>
                 <h1>RACE Your React 🎉</h1>
             </header>
-        </>
+            <Member member={{ firstName: "Peter", lastName: "Lind"}} />
+            <Member member={{ firstName: "Edith", lastName: "Terte" }} />            <member />
+            <Member member={{ firstName: "Oskar", lastName: "Tuska" }} />        </>
     );
 }
 
+function Member({ member }) {
+    return (
+    <article>
+        <h2>{member.firstName}</h2>
+    </article>
+    );
+}
 export default App;
